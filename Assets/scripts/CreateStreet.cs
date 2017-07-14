58276750c8a24e1451ec058fc81148d484e1c948
Alexander Moser <amoser.mmt-b2014@fh-salzburg.ac.at>
2017-07-15 00:39:21 +0200

    old section can be deleted, new section can be created on runtime

--- Assets/scripts/CreateStreet.cs
+++ Assets/scripts/CreateStreet.cs
@@ -11,6 +11,7 @@ public class CreateStreet : MonoBehaviour
     private GameObject[] streetSections;
     private Vector3 distance = new Vector3(0, 0, 10);
     private Vector3 position = new Vector3(0, 0, 0);
+    private int oldestCreatedSection = 0;
 
     private Random rnd = new Random();
 
@@ -22,22 +23,42 @@ public class CreateStreet : MonoBehaviour
 
         for (int i = 0; i < NumberOfVisibleStreetsSections; i++)
         {
-            int j = Mathf.FloorToInt(Random.value * streetPrefabs.Length);
-
-            // Create section
-            GameObject section = (GameObject)Instantiate(streetPrefabs[j]);
-            streetSections[i] = section;
-
-            // Place section
-            position.z += section.GetComponent<MeshCollider>().bounds.size.z / 2;
-            section.transform.position = position;
-            position.z += section.GetComponent<MeshCollider>().bounds.size.z / 2;
+            placeNewSection(i);
         }
+    }
+
+    void placeNewSection(int i)
+    {
+        // Create section
+        int j = Random.Range(0, streetPrefabs.Length);
+
+        GameObject section = (GameObject)Instantiate(streetPrefabs[j]);
+        streetSections[i] = section;
+
+        // Place section
+        position.z += section.GetComponent<MeshCollider>().bounds.size.z / 2;
+        section.transform.position = position;
+        position.z += section.GetComponent<MeshCollider>().bounds.size.z / 2;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        // Player is behind second street section
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("Delete Street Section");
 
+            Destroy(streetSections[oldestCreatedSection]);
+            placeNewSection(oldestCreatedSection);
+
+            oldestCreatedSection++;
+            if (oldestCreatedSection == streetSections.Length)
+            {
+                oldestCreatedSection = 0;
+            }
+
+            Debug.Log("Create Street Section");
+        }
 	}
 }
