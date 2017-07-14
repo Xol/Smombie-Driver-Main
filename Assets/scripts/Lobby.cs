3c59b8c361115cec6339ed5ad03849971983739f
Lars Engel <lars.engel@innogames.com>
2017-07-14 23:01:24 +0200

    Lobby

---
+++ Assets/scripts/Lobby.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lobby : MonoBehaviour {
+
+	void Start() {
+		StartCoroutine (MeteorExample ());
+	}
+		
+
+	IEnumerator MeteorExample() {
+			yield return Meteor.Connection.Connect ("ws://localhost:3000/websocket");
+
+		// Create a collection
+		var collection = new Meteor.Collection<RoomDocumentType> ("rooms");
+
+		// Add some handlers with the new observer syntax
+		var observer = collection.Find ().Observe (
+			added: (string id, RoomDocumentType document) => {
+				Debug.Log(string.Format("Document added: [_id={0}]", document._id));
+			},
+			changed: (string id, RoomDocumentType document, IDictionary changes, string[] deletions) => {
+				Debug.Log(string.Format("Document changed: [_id={0}]", document._id));
+			}
+		);
+
+
+
+		// Subscribe
+		// var subscription = Meteor.Subscription.Subscribe ("subscriptionEndpointName", /*arguments*/ 1, 3, 4);
+		// The convention to turn something into a connection is to cast it to a Coroutine
+		// yield return (Coroutine)subscription;
+
+		// Create a method call that returns a string
+		var methodCall = Meteor.Method<string>.Call ("createRoom");
+		// Execute the method. This will yield until all the database side effects have synced.
+		yield return (Coroutine)methodCall;
+		Debug.Log (methodCall.Response);
+
+		// Get the value returned by the method.
+		// Debug.Log (string.Format ("Method response:\n{0}", methodCall.Response));
+	}
+
+}
+
+public class RoomDocumentType : Meteor.MongoDocument {
+	public string room_id;
+	public bool app_connected;
+}
