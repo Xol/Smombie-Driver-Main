82b2d8a7df204ef00f8d07adc44ecb836f5b73e4
Lars Engel <lars.engel@innogames.com>
2017-07-15 00:03:26 +0200

    Refactoring of meteor class

--- Assets/scripts/Meteor.cs
+++ Assets/scripts/Meteor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lobby : MonoBehaviour {
+public class Meteor : MonoBehaviour {
 	
 	static string room_id;
 
@@ -11,18 +11,21 @@ public class Lobby : MonoBehaviour {
 	}
 
 	void Start() {
-		StartCoroutine (MeteorLobby ());
+		StartCoroutine (MeteorCoroutine ());
 	}
 		
 
-	IEnumerator MeteorLobby() {
-			yield return Meteor.Connection.Connect ("ws://localhost:3000/websocket");
+	IEnumerator MeteorCoroutine() {
+		// connect to meteor
+		yield return Meteor.Connection.Connect ("ws://localhost:3000/websocket");
 
-		// Create a collection
-		var collection = new Meteor.Collection<RoomDocumentType> ("rooms");
+		// Create a collections
+		var rooms = new Meteor.Collection<RoomDocumentType> ("rooms");
+		var points = new Meteor.Collection<PointDocumentType> ("points");
 
-		// Add some handlers with the new observer syntax
-		var observer = collection.Find ().Observe (
+
+		// Add some handlers
+		var room_observer = rooms.Find ().Observe (
 			added: (string id, RoomDocumentType document) => {
 				Debug.Log(string.Format("Document added: [_id={0}]", document._id));
 			},
@@ -34,23 +37,27 @@ public class Lobby : MonoBehaviour {
 			}
 		);
 
-
-
-		// Subscribe
-		// var subscription = Meteor.Subscription.Subscribe ("subscriptionEndpointName", /*arguments*/ 1, 3, 4);
-		// The convention to turn something into a connection is to cast it to a Coroutine
-		// yield return (Coroutine)subscription;
+		var points_observer = points.Find ().Observe (
+			added: (string id, PointDocumentType document) => {
+				Debug.Log(string.Format("Document added: [_id={0}]", document._id));
+			},
+			changed: (string id, PointDocumentType document, IDictionary changes, string[] deletions) => {
+				if (document.room_id == room_id) {
+					Debug.Log("Points: " + document.points);
+				}
+			}
+		);
 
 		// Create a method call that returns a string
 		var methodCall = Meteor.Method<string>.Call ("createRoom");
+
 		// Execute the method. This will yield until all the database side effects have synced.
 		yield return (Coroutine)methodCall;
+
 		room_id = methodCall.Response;
 		Debug.Log ("Room " + room_id + " created.");
 		GameObject.Find ("RoomKey").GetComponent<TextMesh> ().text = room_id;
 
-		// Get the value returned by the method.
-		// Debug.Log (string.Format ("Method response:\n{0}", methodCall.Response));
 	}
 
 }
@@ -58,4 +65,9 @@ public class Lobby : MonoBehaviour {
 public class RoomDocumentType : Meteor.MongoDocument {
 	public string room_id;
 	public bool app_connected;
+}
+
+public class PointDocumentType : Meteor.MongoDocument {
+	public string room_id;
+	public int points;
 }
