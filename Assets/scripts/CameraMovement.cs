26b58c28df765a172be9213dd18b3d4523bd9cdb
Lars Engel <lars.engel@innogames.com>
2017-07-15 22:11:46 +0200

    Worked on Car Movement and Camera

---
+++ Assets/scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float distance_z = 10;
+	public float distance_y = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform car_transform = GameObject.Find ("Car").transform;
+		transform.position = new Vector3(transform.position.x, car_transform.position.y + distance_y, car_transform.position.z - distance_z);
+		
+	}
+}
--- Assets/scripts/OnHittingObstacle.cs
+++ Assets/scripts/OnHittingObstacle.cs
@@ -19,6 +19,6 @@ public class OnHittingObstacle : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-		StartCoroutine(GameObject.Find("__Meteor").GetComponent<MeteorConnector>().ModifyPoints(-10));
+		//StartCoroutine(GameObject.Find("__Meteor").GetComponent<MeteorConnector>().ModifyPoints(-10));
     }
 }
