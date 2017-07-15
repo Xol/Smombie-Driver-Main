321ffdae71566653fd97ed431a87d282bec07b4a
Alexander Moser <amoser.mmt-b2014@fh-salzburg.ac.at>
2017-07-15 13:26:03 +0200

    3 out of 4 Movement Behaviours done

--- Assets/scripts/MovingObstacle.cs
+++ Assets/scripts/MovingObstacle.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class MovingObstacle : MonoBehaviour
 {
-    private Vector3 startPosition;
-    private Vector3 endPosition;
+    private MovementStrategy ms;
 
-    [SerializeField]
-    private bool backAndForward = false;
     [SerializeField]
     private float speed = 1;
-    private Vector3 direction;
+    [SerializeField]
+    private float acceleration = 1;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        startPosition = this.transform.position;
-        endPosition = this.transform.position;
-        endPosition.x *= -1;
-
-        direction = endPosition - startPosition;
-        direction.Normalize();
+        switch (tag)
+        {
+            case "Obstacle - Stationary":
+                ms = new StationaryStrategy(transform);
+                break;
+            case "Obstacle - Linear":
+                ms = new LinearStrategy(transform, speed);
+                break;
+            case "Obstacle - Accelerate":
+                ms = new AccelerateStrategy(transform, acceleration);
+                break;
+            case "Obstacle - Feint":
+                ms = new FeintStrategy(transform);
+                break;
+            default:
+                ms = new StationaryStrategy(transform);
+                break;
+        }
     }
 	
 	// Update is called once per frame
@@ -30,22 +40,117 @@ public class MovingObstacle : MonoBehaviour
         // Player is close enough
         if (true)
         {
-            // Moving to the right
-            if ((direction.x < 0 && this.transform.position.x > endPosition.x) ||
-                (direction.x > 0 && this.transform.position.x < endPosition.x))
-            {
-                this.transform.position += direction * speed;
-            }
-            else
-            {
-                this.transform.position = endPosition;
-
-                if (backAndForward)
-                {
-                    endPosition.x *= -1;
-                    direction *= -1;
-                }
-            }
+            ms.MoveUpdate(Time.deltaTime);
         }
+    }
+}
+
+
+/// <summary>
+/// Movement Strategy
+/// </summary>
+
+
+public class MovementStrategy
+{
+    protected Vector3 startPosition;
+    protected Vector3 endPosition;
+    protected Vector3 direction;
+    protected Transform transform;
+
+    public MovementStrategy (Transform tf)
+    {
+        transform = tf;
+
+        startPosition = transform.position;
+        endPosition = transform.position;
+        endPosition.x *= -1;
+
+        direction = endPosition - startPosition;
+        direction.Normalize();
+    }
+
+    public virtual void MoveUpdate (float deltaTime)
+    {
+        // Do Something...
+    }
+}
+
+public class StationaryStrategy : MovementStrategy
+{
+    public StationaryStrategy(Transform transform)
+        : base(transform)
+    {
+
+    }
+
+    public override void MoveUpdate(float deltaTime)
+    {
+        // Do Nothing
+    }
+}
+
+public class LinearStrategy : MovementStrategy
+{
+    protected float velocity;
+
+    public LinearStrategy(Transform transform, float v)
+        : base(transform)
+    {
+        velocity = v;
+    }
+
+    public override void MoveUpdate(float deltaTime)
+    {
+        if ((direction.x < 0 && transform.position.x > endPosition.x) ||
+            (direction.x > 0 && transform.position.x < endPosition.x))
+        {
+            transform.position += direction * velocity * deltaTime;
+        }
+        else
+        {
+            transform.position = endPosition;
+        }
+    }
+}
+
+public class AccelerateStrategy : MovementStrategy
+{
+    protected float acceleration;
+    protected float velocity;
+
+    public AccelerateStrategy(Transform transform, float a)
+        : base(transform)
+    {
+        acceleration = a;
+        velocity = 0;
+    }
+
+    public override void MoveUpdate(float deltaTime)
+    {
+        if ((direction.x < 0 && transform.position.x > endPosition.x) ||
+            (direction.x > 0 && transform.position.x < endPosition.x))
+        {
+            velocity += acceleration * deltaTime;
+            transform.position += direction * velocity * deltaTime;
+        }
+        else
+        {
+            transform.position = endPosition;
+        }
+    }
+}
+
+public class FeintStrategy : MovementStrategy
+{
+    public FeintStrategy(Transform transform)
+        : base(transform)
+    {
+
+    }
+
+    public override void MoveUpdate(float deltaTime)
+    {
+        // Do Nothing
     }
 }
