3ec968d3f2b753fd9acf2fa4f679ba5e917357e2
Lars Engel <lars.engel@innogames.com>
2017-07-16 10:33:20 +0200

    Success notification

---
+++ Assets/scripts/AnimateNotificationSuccess.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateNotificationSuccess : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public IEnumerator ShowSuccess() {
+		transform.localScale = new Vector3 (0.3f, 0.3f, 1);
+		yield return new WaitForSeconds(4);
+		transform.localScale = Vector3.zero;
+	}
+}
